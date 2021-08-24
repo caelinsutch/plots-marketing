8ed6f6f19f7f6b4caae8dbf4bd45e35bd6f27ecc
Caelin Sutch <caelinsutch@gmail.com>
2021-08-24 15:37:59 -0700

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 
 import PageContainer from '../Components/PageContainer';
+import { Box, Flex, Text, Link as ChakraLink } from '@chakra-ui/react';
+import { Link } from 'gatsby';
 
 const NotFoundPage = () => (
   <PageContainer>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Flex
+      height="100vh"
+      width="100%"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box textAlign="center">
+        <Text fontSize="4xl" fontWeight="bold">
+          404 Not Found
+        </Text>
+        <ChakraLink textDecoration="underline" fontSize="lg">
+          <Link to="/">Go Home</Link>
+        </ChakraLink>
+      </Box>
+    </Flex>
   </PageContainer>
 );
 
